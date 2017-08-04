7ad0fff63c9c36f7a39b3d4c6006ae680187fe44
Rebecca Senft <senft@g.harvard.edu>
2017-08-04 10:12:27 -0400

    RAS added etPCAdemo_ng.m

---
+++ Unit #2 Bootstrap 1/etPCAdemo_ng.m
@@ -0,0 +1,264 @@
+% etPCAdemo.m: Bootstrap calculation of std. error for PCA results
+%
+% Instructions:
+% The goal of this exercise is to become familiar with the technique of
+% bootstrapping and appreciate how it can be used to estimate accuracy of 
+% statistics through resampling data to generate standard errors and
+% confidence intervals that may otherwise be difficult to compute directly.
+
+% What to do: Each section begins with a '%%'. Read through the comments
+% and follow the instructions provided. In some cases you will be asked to
+% answer a question, clearly indicated by 'QUESTION'--please do so using
+% comments (i.e., by starting each line with a '%'). In other cases, you be
+% asked to supply missing code, indicated by 'TODO'. Once you have supplied
+% the required code, you can execute that section by mouse-clicking in that
+% section (The block will turn yellow.) and then simultaneously hitting the
+% 'ctrl' and 'enter' keys (PC) or 'command' and 'enter' keys (Mac).
+%
+% The data here are students' scores across tests in different subjects
+% from Mardia, Kent and Bilby (1979)
+% n = 88 students (rows) each took 5 tests (columns), in: 
+% Mechanics, Vectors, Algebra, Analysis and Statistics. 
+% The first two tests were closed book, the last three open book.
+
+% Principal components analysis (PCA) is a useful tool in multivariate
+% analysis, as we often have several variables that are correlated with one
+% another. PCA allows us to reduce the dimensions of our data by
+% transforming our data into orthogonal 'principal components' which
+% are uncorrelated and contain the information of the original dataset
+% (but may be difficult to interpret). The principal components, 
+% or eigenvectors if you've taken linear algebra, are linear combinations 
+% of the original variables of the dataset weighted by their contribution 
+% to explaining the data's variance in a particular orthogonal dimension,
+% chosen such that the first PC explains the most variance possible, the
+% second the next most possible in an orthogonal direction to the first,
+% and so on.
+
+% Original source of exercise: Efron, B. & Tibshirani Robert, J. (1993) 
+% An introduction to the bootstrap. Chapman & Hall, London u.a. pp. 61-70 
+% Adapted by RTB 5/31/2002, originally named bs_ex2.m
+% Developed for homework by RAS and RTB, August 2017
+%% Concepts covered:
+%1. z-scoring to see relationships among different measures
+%2. PCA to capture maximal variance ('scientific IQ')
+%3. bootstrap to estimate std. error of our PCA-based statistic
+%4. bootstrap for confidence intervals: std. normal vs. percentile
+
+%% Load and plot data
+nBoot = 200;
+myAlpha = 0.05;
+
+load MardiaTestScores.mat
+X = [Mechanics,Vectors,Algebra,Analysis,Statistics];
+[nStudents,nTests] = size(X);
+
+% Plot the raw data to get a sense of the correlations:
+figure, plot(X);
+xlabel('Student ID#'); ylabel('raw test score');
+legend('Mechanics','Vectors','Algebra','Analysis','Statistics');
+title('Raw test scores from Mardia et al. 1979');
+
+% We can do even better by first z-scoring the data
+% Note that zscore operates on columns by default, so we are essentially
+% normalizing each students score within each test
+Z = zscore(X);
+figure, plot(Z);
+xlabel('Student ID#'); ylabel('z-scored test score');
+legend('Mechanics','Vectors','Algebra','Analysis','Statistics');
+title('Z-scored test scores from Mardia et al. 1979');
+
+% QUESTION: Looking at Figure 1 and Figure 2, does there seem to be any
+% relationship between a student's scores on the different tests? Do
+% students who do well on one test tend to do well on others?
+%% How well correlated are students on two randomly chosen tests
+
+testNames = {'Mechanics','Vectors','Algebra','Analysis','Statistics'};
+% TO DO: We want to randomly choose two tests to plot against each other.
+% Define testX and testY to be variables that will a random whole number
+% from 1 to the number of tests (this information is stored in the variable
+% nTests).
+testX = unidrnd(nTests);
+testY = unidrnd(nTests);
+
+% TO DO: Write a single line of code to plot the correlation between two
+% randomly selected tests. Use testX and testY to index into the main data
+% array X. Make the points black and open circles. The lines after will set
+% up axes and titles.
+figure, plot(X(:,testX),X(:,testY),'ko');
+
+xStr = sprintf('Score on test in %s', testNames{testX});
+xlabel(xStr);
+yStr = sprintf('Score on test in %s', testNames{testY});
+ylabel(yStr);
+lsline; % plot least squares regression line
+title('Correlation between two randomly selected tests');
+
+%% Also look at correlation matrix:
+rhoCols = corr(X);
+figure, imagesc(rhoCols,[0,1]); colorbar;
+xlabel('Test'); ylabel('Test');
+title('Correlation matrix for test scores');
+set(gca, 'XTick', 1:nTests); % center x-axis ticks on bins
+set(gca, 'YTick', 1:nTests); % center y-axis ticks on bins
+set(gca, 'XTickLabel', testNames);
+set(gca, 'YTickLabel', testNames);
+
+% QUESTION: Which tests appear to be the most correlated with one another?
+% In general, are the test scores highly correlated with one another?
+
+%% PCA to capture sources of maximal variance
+% We first note that the test scores are highly correlated: a student who
+% did well on one test also tended to well on the others. In the limit,
+% each student (x) could be captured by a single number, Q, that would
+% completely describe his performance: x_i = Q_i * v
+% We could think of Q as each student's scientific IQ.
+% In the language of linear algebra (PCA), v is the eigenvector of the
+% covariance matrix (the first principle component) and Q_i is the distance
+% along the line defined by the eigenvector for each student. The
+% eigenvalues reflect the amount of variance explained by each eigenvector,
+% so the ratio of the 1st eigenvalue to the sum of all the eigenvalues
+% gives the fraction of variance explained by the first principle
+% component. Thus it tells us how much of the variability among the
+% different stuudents can be explained by a single number, the IQ.
+
+% Calculate the observed value for the variance explained by the first
+% principle component:
+% coef: the coefficients for the principal components; 1st PC is column 1, etc.
+% score: the representations of X in the principal component space
+% latent: the eigenvalues of the covariance matrix; related to variance
+% explained by each PC
+[coef,score,latent] = pca(X);
+
+% TO DO: Using the knowledge above, assign the variable thHat 
+% to be the proportion of the total variance explained by the 1st PC. What
+% is this percentage?
+thHat = latent(1) / sum(latent);    % 0.6191
+
+%% Bootstrap to calculate standard error of our measure.
+% So the first PC explains 62% of the test score variance (the answer to 
+% the question above!), which is pretty darned good. But how precise is 
+% this value? There is no simple formula for this, like there is for the 
+% std. error of the mean, but we can do it with the bootstrap by taking 
+% random samples of the rows, with replacement:
+thBoot = zeros(nBoot,1);
+coefBoot = zeros(nTests,nTests,nBoot);
+for k = 1:nBoot
+    % TO DO: write a single line of code to randomly sample the rows of our
+    % dataset X with replacement. The resulting variable bootSamp, will be
+    % an 88x5 double.
+    % HINT: This requires careful indexing of our dataset X and can be 
+    % accomplished with the use of the unidrnd function to draw numbers
+    % from a random uniform distribution with the maximum value set at the
+    % number of students.
+    bootSamp = X(unidrnd(nStudents,nStudents,1),:);
+    
+    [bsCoef,~,bsLatent] = pca(bootSamp);
+    thBoot(k) = bsLatent(1) / sum(bsLatent);
+    % also save the coefficients for the PCs
+    coefBoot(:,:,k) = bsCoef;
+end
+figure, hist(thBoot);
+hold on
+xlabel('Fraction of variance explained by 1st PC');
+ylabel('#');
+title('Bootstrap replicates of proportion of the total variance explained by the 1st PC');
+thSE = std(thBoot);
+ax = axis;
+line([thHat,thHat],[ax(3),ax(4)],'Color','y');
+
+%% Bootstrap of confidence intervals
+% There are two ways to compute confidence intervals with the bootstrap.
+% One is to do a huge number of bootstrap replicates and then sort them and
+% find the cut-offs. E.g. If we do 1000 replicates, and we want the 95% CI,
+% we sort and find the 25th and 975th values.
+% The other way is to use asymptotic normal distribution theory. Recall
+% that the std. error IS a confidence interval, the 68% CI. So to get any
+% other CI, we just calculate the appropriate number of standard deviates
+% from the normal distribution:
+dev = thSE * norminv(1-myAlpha/2);
+thCI = [thHat - dev, thHat + dev];
+h1 = line([thCI(1),thCI(1)],[ax(3),ax(4)],'Color','r');
+line([thCI(2),thCI(2)],[ax(3),ax(4)],'Color','r');
+
+% QUESTION: How would you modify the above code if you wanted to get a 99%
+% CI?
+% change myAlpha to 0.01
+
+%% Here is the brute force way: percentile method
+thBootSorted = sort(thBoot);
+iLo = ceil((myAlpha/2) * nBoot);   % index corresponding to lower bound
+iHi = nBoot - iLo;                  % index corresponding to upper bound
+thCI2 = [thBootSorted(iLo),thBootSorted(iHi)];
+h2=line([thCI2(1),thCI2(1)],[ax(3),ax(4)],'Color','g');
+line([thCI2(2),thCI2(2)],[ax(3),ax(4)],'Color','g');
+
+legend([h1,h2],'Standard Normal CI','Percentile CI');
+% QUESTION: How similar are the standard normal CI and percentile CIs? 
+% Why might they be slightly different? 
+% 
+% They are very close, but a very slight difference may come from the fact
+% the the percentile method chooses values from the sorted sample 
+% variances that represent the 2.5th and 97.5th percentiles, 
+% limiting the possible values to those in the dataset, while the normal
+% method uses a normal inverse cumulative distribution function to
+% calculate a deviance from an estimate of the standard error thHat. The
+% interval is then the estiamte plus or minus this deviance. This
+% calculation doesn't limit the CI values found in the sampled variances.
+
+%% We can also look at the variability of the PC coefficients themselves:
+figure,
+h1=plot(squeeze(coefBoot(:,1,:)),'k-');         % 1st PC
+hold on
+h2=plot(squeeze(coefBoot(:,2,:)),'r-');            % 2nd PC
+h3=plot(squeeze(coefBoot(:,3,:)),'g-');            % 3rd PC
+legend([h1(1),h2(1),h3(1)],'PC1','PC2','PC3');
+xlabel('Coefficient'); ylabel('Coefficient value');
+title('Bootstrap replicates of 1st three PCs');
+
+% QUESTION: Looking at the plot, what PCs are most variable? Is there a 
+% pattern to this variation? 
+% The PCs are eigenvectors, which are still valid when sign-inverted. 
+% The pattern matters more here, and as a somewhat aribtrary choice is 
+% made regarding sign, we get mirror-image bimodality in the bootstrapped
+% PCs.
+
+%% Computation of the PC's.
+% Start with the covariance matrix:
+G = cov(X);
+% The PC are obtained as the eigenvectors of the covariance matrix:
+[V,D] = eig(G);
+
+% The eigenvalues give a relative measure of the variance explained by each
+% PC. In MATLAB's PCA-speak, this is returned as 'latent'. But we can also
+% get it as:
+lambda = diag(D);   % lambda for the 1st PC is lambda(5), seemingly backwards
+
+% For E&T p. 67-8 on interpretation of PCs:
+
+% TO DO: Compute the first eigenvector (the first PC) by taking the last
+% column of the matrix V. Transpose this into a horizontal vector. Also
+% take the second eigenvector (the second to last column).
+
+% First eigenvector (PC1) = V(:,end)' = [0.505,0.368,0.3456,0.451,0.535]
+% Second eigenvector (PC2) = V(:,end-1)' = [-0.75,-0.21,0.08,0.30,0.55]
+
+% If we wanted to best summarize each student's performance with one
+% number, we would multiply each test score by the corresponding
+% coefficient and sum up the result. This is called the 'loading':
+loadPC1 = X(:,:) * V(:,5);  % Scientific IQ for each of the 88 students
+loadPC2 = X(:,:) * V(:,4);  % loading onto 2nd PC
+
+% QUESTION: The weights assigned by PCA can tell us about the structure of
+% the multivariate data set. Interpretation of the principal components
+% can sometimes be challenging. The 1st PC puts positive and roughly equal 
+% weights on each of the five tests, so 'loadPC1' can be thought of as the 
+% sum (or average) of each student's 5 test scores. Look at the weights in  
+% PC2. What does PC2 represent? What does it mean for a student to have a 
+% high'loadPC2'?
+
+% Ans: PC2 puts negative weights on the first 2 (closed book) tests and  
+% positive weights on the 3 open-book tests, so loadPC2 is a contrast  
+% between a student's open and close book performances. A student with a  
+% high 'loadPC2' did much better on the open book tests than on the closed  
+% book tests.
+

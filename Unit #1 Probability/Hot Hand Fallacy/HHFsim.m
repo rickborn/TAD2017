6c4fda506cc8a15131a51abc5ba948d9bdc831e7
Rick Born <richard_born@hms.harvard.edu>
2020-06-16 08:25:09 -0700

    Update HHFsim.m
    Spiffing up the hot hand fallacy code to include axis labels, etc.

--- Unit #1 Probability/Hot Hand Fallacy/HHFsim.m
+++ Unit #1 Probability/Hot Hand Fallacy/HHFsim.m
@@ -5,8 +5,7 @@
 % Inspired by this Data Colada post by Uri Simonsohn:
 % http://datacolada.org/88
 %
-% My favorite fact for this post: calibration of massiveness of hot hand
-% fallacy
+% My favorite fact for this post: calibration of massiveness of hot hand fallacy
 % It is worth noting that expecting r=.40 for consecutive shots
 % (see chart above) implies implausibly strong hand-hotness. As a
 % calibration, I simulated a player that when cold (previous shot was
@@ -19,6 +18,11 @@
 % for two consecutive shots
 
 %% First let's calculate the simulated expectation under the null
+
+% We are looking at the degree of correlation for consecutive shots. That
+% is, for a sequence of 100 shots, the overall correlation between the two
+% vectors of 0s and 1s corresponding to shots 1:99 and 2:100, or, in MATLAB
+% speak, if our vector is X: corr([X(1:end-1),X(2:end)])
 
 nSims = 10000;
 nShots = 100;
@@ -33,7 +37,10 @@ end
 
 figure
 histogram(allR);
-mean(allR)          % should be around 0
+xlabel('Correlation between consecutive shots');
+ylabel('# of simulations');
+title('Null Distribution for Hot Hand Fallacy');
+nullMean = mean(allR);          % should be around 0
 
 %% Now generate data according to Simonsohn's rule above
 
@@ -66,8 +73,13 @@ for k = 1:nSims
     allR(k,1) = r(2,1);
 end
 
+HHmean = mean(allR);
+
 figure
 histogram(allR);
+xlabel('Correlation between consecutive shots');
+ylabel('# of simulations');
+title('Distribution for ''Extreme Hot Handedness''');
 hold on
 
 % If I use Simonsohn's values for pHit, I get the same answer of a mean
@@ -79,14 +91,18 @@ hold on
 myAlpha = 0.05;
 
 sortedR = sort(allR);
-[idxLo,idxHi] = getCiIdx(myAlpha,nSims);
+[idxLo,idxHi] = getCIidx(myAlpha,nSims);
 ciLo = sortedR(idxLo);
 ciHi = sortedR(idxHi);
 
 ax = axis;
+xT = ceil(ax(1) * 10) / 10;
+yT = ax(4) - (0.1 * ax(4));
+text(xT,yT,['Mean corr = ', num2str(HHmean,3)]);
 
+h0 = line([HHmean, HHmean],[ax(3), ax(4)],'Color','y');
 h1 = line([ciLo, ciLo],[ax(3), ax(4)],'Color','r');
 h2 = line([ciHi, ciHi],[ax(3), ax(4)],'Color','r');
-
+legend(h1,'95% CI');
 % Note that the uppper 95% CI is around 0.41, which is the overestimated
 % average in the GVT study #4.
